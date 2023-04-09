b96b08aa8bd151bc690f918e7056e45845a3240d
Mert Duzgun <duzgunnmert@gmail.com>
2023-04-09 22:07:36 +0300

    Create basic Button stories

---
+++ src/components/Button/Button.stories.ts
@@ -0,0 +1,52 @@
+import '../../index.css';
+
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './Button';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
+const meta = {
+  title: 'Button',
+  component: Button,
+  tags: ['autodocs'],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Button',
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Button',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+    children: 'Button',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+    children: 'Button',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Button',
+    disabled: true,
+  },
+};
