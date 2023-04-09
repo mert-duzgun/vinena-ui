bd171945acf4a5a9397a8a8d024c195e56f639c1
Mert Duzgun <duzgunnmert@gmail.com>
2023-04-09 20:49:50 +0300

    Create button with styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,62 @@ function App() {
   return (
     <div className="p-4">
       <h1>Vinena UI</h1>
-      <Button />
+      <div className="space-x-4">
+        <Button size="small">Button</Button>
+        <Button>Button</Button>
+        <Button size="large">Button</Button>
+      </div>
+      <div className="mt-4 space-x-4">
+        <Button variant="secondary" size="small">
+          Button
+        </Button>
+        <Button variant="secondary">Button</Button>
+        <Button variant="secondary" size="large">
+          Button
+        </Button>
+      </div>
+      <div className="mt-4 space-x-4">
+        <Button disabled size="small">
+          Button
+        </Button>
+        <Button disabled>Button</Button>
+        <Button disabled size="large">
+          Button
+        </Button>
+      </div>
+      <div className="mt-4 space-x-4">
+        <Button disabled variant="secondary" size="small">
+          Button
+        </Button>
+        <Button disabled variant="secondary">
+          Button
+        </Button>
+        <Button disabled variant="secondary" size="large">
+          Button
+        </Button>
+      </div>
+      <div className="mt-4 space-x-4">
+        <Button as="a" href="https://duzgun.dev" target="_blank" size="small">
+          Button
+        </Button>
+        <Button as="a" href="https://duzgun.dev" target="_blank">
+          Button
+        </Button>
+        <Button as="a" href="https://duzgun.dev" target="_blank" size="large">
+          Button
+        </Button>
+      </div>
+      <div className="mt-4 space-x-4">
+        <Button as="a" href="https://duzgun.dev" target="_blank" variant="secondary" size="small">
+          Button
+        </Button>
+        <Button as="a" href="https://duzgun.dev" target="_blank" variant="secondary">
+          Button
+        </Button>
+        <Button as="a" href="https://duzgun.dev" target="_blank" variant="secondary" size="large">
+          Button
+        </Button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,64 @@
+import clsx from 'clsx';
+import { AnchorHTMLAttributes, ButtonHTMLAttributes, ReactNode } from 'react';
+
+interface ButtonProps {
+  variant?: 'primary' | 'secondary';
+  as?: 'button' | 'a';
+  size?: 'small' | 'default' | 'large';
+  className?: string;
+  icon?: ReactNode;
+}
+
+export const Button = ({
+  children,
+  className,
+  variant = 'primary',
+  as = 'button',
+  size = 'default',
+  ...rest
+}: ButtonProps &
+  ButtonHTMLAttributes<HTMLButtonElement> &
+  AnchorHTMLAttributes<HTMLAnchorElement>) => {
+  let baseStyle, textSize;
+  switch (size) {
+    case 'small':
+      baseStyle = 'px-4 py-2';
+      textSize = 'text-sm';
+      break;
+    case 'large':
+      baseStyle = 'px-4 py-3.5';
+      textSize = 'text-lg';
+      break;
+    default:
+      baseStyle = 'px-4 py-3';
+      textSize = 'text-base';
+  }
+  const baseStyleClasses = `${baseStyle} inline-flex border transition rounded focus:outline-none focus:ring-[3px] focus:ring-primary-300`;
+  let colorStyle;
+  switch (variant) {
+    case 'primary':
+      colorStyle =
+        'text-white bg-primary-500 border-primary-500 hover:bg-primary-700 hover:border-primary-700 disabled:bg-grayscale-300 disabled:border-grayscale-300';
+      break;
+    case 'secondary':
+      colorStyle =
+        'text-primary-500 bg-transparent border-primary-500 hover:bg-primary-50 disabled:text-grayscale-300 disabled:border-grayscale-300 disabled:bg-transparent';
+      break;
+  }
+
+  const classes = clsx(baseStyleClasses, textSize, colorStyle, className);
+
+  if (as === 'a') {
+    return (
+      <a className={clsx(classes, 'cursor-pointer')} {...rest}>
+        {children}
+      </a>
+    );
+  } else {
+    return (
+      <button className={classes} type="button" {...rest}>
+        {children}
+      </button>
+    );
+  }
+};
