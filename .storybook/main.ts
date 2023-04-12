dc7736b0787563286896e394d732f8d9a1869284
Mert Duzgun <duzgunnmert@gmail.com>
2023-04-12 11:28:22 +0300

    Remove unnecessary vite configs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,5 +1,4 @@
 import type { StorybookConfig } from '@storybook/react-vite';
-import { mergeConfig, splitVendorChunkPlugin } from 'vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -14,24 +13,6 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag',
-  },
-  async viteFinal(config) {
-    return mergeConfig(config, {
-      build: {
-        sourcemap: false,
-        target: ['es2020'],
-        rollupOptions: {
-          output: {
-            manualChunks: {
-              react: ['react', 'react-dom'],
-              lodash: ['lodash'],
-            },
-            cache: false,
-          },
-        },
-      },
-      plugins: [splitVendorChunkPlugin()],
-    });
   },
 };
 export default config;
