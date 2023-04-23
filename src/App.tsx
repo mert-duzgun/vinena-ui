d402c783d346692fbd39dc225596d845069b375a
Mert Duzgun <duzgunnmert@gmail.com>
2023-04-23 19:17:01 +0300

    Add emotion global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import { ThemeProvider } from '@emotion/react';
 import { Box, Button } from 'components';
-import { DEFAULT_THEME } from 'theme';
+import { DEFAULT_THEME, GlobalStyles, rem } from 'theme';
 
 function App() {
   return (
     <ThemeProvider theme={DEFAULT_THEME}>
-      <div className="p-4">
+      <div css={{ padding: rem(16) }}>
         <h1>Vinena UI</h1>
-        <Box>Merhaba</Box>
-        <div css={{ display: 'flex', gap: '30px' }}>
-          <Button size="small">Button</Button>
-          <Button>Button</Button>
+        <Box css={{ marginBottom: rem(16) }}>Merhaba</Box>
+        <div css={{ display: 'flex', gap: rem(32) }}>
           <Button size="large">Button</Button>
+          <Button>Button</Button>
+          <Button size="small">Button</Button>
         </div>
       </div>
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,2 +1,3 @@
+export * from './GlobalStyles';
 export * from './theme';
 export * from './utils';
