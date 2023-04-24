f0113797f9ac0cdb8f91861e1df59c0884e3cf01
Mert Duzgun <duzgunnmert@gmail.com>
2023-04-24 12:55:06 +0300

    Fix button ts bug

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -8,31 +8,29 @@ import useStyles from './Button.styles';
 export type ButtonProps<C extends React.ElementType> = PolymorphicComponentProp<
   C,
   {
-    className?: string;
     priority?: 'primary' | 'secondary' | 'tertiary';
+    size?: 'small' | 'default' | 'large';
     negative?: boolean;
     disabled?: boolean;
-    size?: 'small' | 'default' | 'large';
-    component?: 'a' | 'button';
+    className?: string;
   }
 >;
 
 export const Button = <C extends React.ElementType = 'button'>({
-  children,
-  className,
   priority = 'primary',
+  size = 'default',
   negative = false,
   disabled = false,
-  size = 'default',
-  component = 'button',
+  children,
+  className,
   ...rest
 }: ButtonProps<C>) => {
   const theme = useTheme();
-  const css = useStyles(theme, { priority, negative, size, disabled });
+  const css = useStyles(theme, { priority, size, negative, disabled });
 
   return (
     <Box
-      as={component}
+      as="button"
       className={className}
       css={css.root}
       data-negative={negative}
