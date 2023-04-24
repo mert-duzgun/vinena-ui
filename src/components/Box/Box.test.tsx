dbb9c9a3d51cb40b12f6f79781e98db2d684afb8
Mert Duzgun <duzgunnmert@gmail.com>
2023-04-24 12:54:43 +0300

    Add box tests

---
+++ src/components/Box/Box.test.tsx
@@ -0,0 +1,25 @@
+import { render } from '@testing-library/react';
+
+import { Box } from './Box';
+
+describe('Box', () => {
+  it('should match snapshot', async () => {
+    const { container } = render(<Box>Test</Box>);
+    await expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders a div by default', () => {
+    const { container } = render(<Box>Test</Box>);
+    expect(container.firstChild).toBeInstanceOf(HTMLDivElement);
+  });
+
+  it('renders the component with custom props', () => {
+    const { container } = render(
+      <Box as="span" className="test">
+        Test
+      </Box>
+    );
+    expect(container.firstChild).toHaveClass('test');
+    expect(container.firstChild).toBeInstanceOf(HTMLSpanElement);
+  });
+});
