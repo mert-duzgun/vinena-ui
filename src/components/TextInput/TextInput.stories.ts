3c1c1187f1da54955a4975613a71a744d7e41e89
Mert Duzgun <duzgunnmert@gmail.com>
2023-05-19 19:27:57 +0300

    Add more stories for TextInput

--- src/components/TextInput/TextInput.stories.ts
+++ src/components/TextInput/TextInput.stories.ts
@@ -18,3 +18,63 @@ export const Default: Story = {
     placeholder: 'Placeholder',
   },
 };
+
+export const WithLabel: Story = {
+  args: {
+    placeholder: 'Placeholder',
+    label: 'Label',
+  },
+};
+
+export const WithHelper: Story = {
+  args: {
+    placeholder: 'Placeholder',
+    helperText: 'Helper text',
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    placeholder: 'Placeholder',
+    label: 'Label',
+    helperText: 'Helper text',
+    disabled: true,
+  },
+};
+
+export const Error: Story = {
+  args: {
+    placeholder: 'Placeholder',
+    label: 'Label',
+    helperText: 'Helper text',
+    error: true,
+  },
+};
+
+export const WithLeftIcon: Story = {
+  args: {
+    placeholder: 'Placeholder',
+    label: 'Label',
+    helperText: 'Helper text',
+    leftIcon: 'search',
+  },
+};
+
+export const WithRightIcon: Story = {
+  args: {
+    placeholder: 'Placeholder',
+    label: 'Label',
+    helperText: 'Helper text',
+    rightIcon: 'arrowRight',
+  },
+};
+
+export const WithBothIcons: Story = {
+  args: {
+    placeholder: 'Placeholder',
+    label: 'Label',
+    helperText: 'Helper text',
+    leftIcon: 'search',
+    rightIcon: 'arrowRight',
+  },
+};
